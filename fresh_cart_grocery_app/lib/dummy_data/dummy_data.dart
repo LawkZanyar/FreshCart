39cedda628da6bf825a4779834c405cc65e109ab
LawkZanyar <lzqiu230018@uniq.edu.qiu>
2025-12-01 11:22:57 +0300

    .

--- fresh_cart_grocery_app/lib/dummy_data/dummy_data.dart
+++ fresh_cart_grocery_app/lib/dummy_data/dummy_data.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 final List<Map<String,dynamic>> dummyProducts = [
   {'id':'p1','name':'Organic Apples','price':2.99,'unit':'kg','icon':Icons.apple},
   {'id':'p2','name':'Ripe Bananas','price':1.49,'unit':'doz','icon':Icons.set_meal},
