7e9c3464f50d097142e91d46d743e7d6965dabef
Pashamariwan <pmqiu230163@uniq.edu.iq>
2025-12-01 11:14:48 +0300

    SCRUM-29 Create Product Model

---
+++ fresh_cart_grocery_app/lib/dummy_data/dummy_data.dart
@@ -0,0 +1,5 @@
+final List<Map<String,dynamic>> dummyProducts = [
+  {'id':'p1','name':'Organic Apples','price':2.99,'unit':'kg','icon':Icons.apple},
+  {'id':'p2','name':'Ripe Bananas','price':1.49,'unit':'doz','icon':Icons.set_meal},
+  {'id':'p3','name':'Carrot Pack','price':1.99,'unit':'bag','icon':Icons.grass},
+];
