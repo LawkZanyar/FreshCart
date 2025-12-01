82fc57146409013c529f51261a437b4c9e573fc1
LawkZanyar <lzqiu230018@uniq.edu.qiu>
2025-12-01 15:02:22 +0300

    .

---
+++ fresh_cart_grocery_app/lib/dummy_data/shop_and_product_data.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final List<Map<String,dynamic>> dummyShops = [
+  {'id':'s1','name':'Green Mart','logo':Icons.store},
+  {'id':'s2','name':'Freshly','logo':Icons.local_grocery_store},
+  {'id':'s3','name':'Veggie Hub','logo':Icons.storefront},
+];
+
+final List<Map<String,dynamic>> dummyProducts = [
+  {'id':'p1','name':'Organic Apples','price':2.99,'unit':'kg','icon':Icons.apple},
+  {'id':'p2','name':'Ripe Bananas','price':1.49,'unit':'doz','icon':Icons.set_meal},
+  {'id':'p3','name':'Carrot Pack','price':1.99,'unit':'bag','icon':Icons.grass},
+];
