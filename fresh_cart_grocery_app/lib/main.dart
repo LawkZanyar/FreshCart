bc076b82b72c5cfa4ef7119bbdad75bccc9b9b22
LawkZanyar <lzqiu230018@uniq.edu.qiu>
2026-01-12 12:59:56 +0300

    SCRUM-44 Install and configure firebase SDK

--- fresh_cart_grocery_app/lib/main.dart
+++ fresh_cart_grocery_app/lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_freshcart/services/cart_service.dart';
+import 'package:flutter_freshcart/services/auth_services.dart';
+import 'package:flutter_freshcart/screens/login_screen.dart';
+import 'package:flutter_freshcart/screens/customer_bottom_shell.dart';
 import 'routes/routes.dart';
 import 'package:provider/provider.dart';
 import 'theme/theme.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
       statusBarColor: Colors.transparent, 
@@ -16,8 +29,11 @@ void main() {
 
 
   runApp(
-    ChangeNotifierProvider<CartService>(
-      create: (_) => CartService(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthService>(create: (_) => AuthService()),
+        ChangeNotifierProvider<CartService>(create: (_) => CartService()),
+      ],
       child: const ShopApp(),
     ),
   );
@@ -28,12 +44,26 @@ class ShopApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final auth = context.watch<AuthService>();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shop App',
       theme: appTheme,
       initialRoute: AppRoutes.splash,
       onGenerateRoute: AppRoutes.onGenerateRoute,
+      home: StreamBuilder<User?>(
+        stream: auth.authStateChanges,
+        builder: (_, snap) {
+          if (snap.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+          final user = snap.data;
+          if (user == null) return const LoginScreen();
+          return const CustomerBottomShell();
+        },
+      ),
     );
   }
 }
